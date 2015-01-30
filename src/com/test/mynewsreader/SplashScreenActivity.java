bce05d1484e96e4641b0b00a8f7f361e2a85f565
redturbo5 <jonathanhindharta@gmail.com>
2015-01-30 15:12:23 +0700

    menambah splash screen

---
+++ src/com/test/mynewsreader/SplashScreenActivity.java
@@ -0,0 +1,43 @@
+//Activity untuk Splash Screen
+package com.test.mynewsreader;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+
+public class SplashScreenActivity extends Activity {
+ 	private long ms=0;
+    //waktu tayang splashscreen 3000 millisecond = 3 detik 
+    private long splashTime = 3000;
+    private boolean splashActive = true;
+    private boolean paused = false;
+ 
+
+@SuppressLint("NewApi")
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.splashscreen);
+	getActionBar().hide();
+	Thread mythread = new Thread(){
+        public void run(){
+            try {
+                while (splashActive && ms < splashTime){
+                    if(!paused)
+                        ms=ms+100;
+                    sleep(100);
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            } finally {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        }
+    };
+    mythread.start();
+}
+
+
+}
